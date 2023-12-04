1325dd414ce9203139c699bb26cad5ada6df5f87
Dhiraj Chauhan <chauhandhiraj40@gmail.com>
2023-12-05 00:02:43 +0530

    fix aspect ratio of image

--- app/src/main/java/com/getcode/view/main/getKin/BuyAndSellKin.kt
+++ app/src/main/java/com/getcode/view/main/getKin/BuyAndSellKin.kt
@@ -1,11 +1,11 @@
 package com.getcode.view.main.getKin
 
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,41 +14,30 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.getcode.R
 import com.getcode.view.components.ButtonState
 import com.getcode.view.components.CodeButton
 import com.getcode.view.components.NoRippleIndicationWrapper
-
-data class BuyAndSellKinItem(
-    val imageResId: Int,
-    val buttonTextResId: Int,
-    val link: Uri
-)
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun BuyAndSellKin() {
     val context = LocalContext.current
+    val viewModel = hiltViewModel<BuyAndSellKinViewModel>()
 
-    val items = listOf(
-        BuyAndSellKinItem(
-            imageResId = R.drawable.video_buy_kin_2x,
-            buttonTextResId = R.string.action_learnHowToBuyKin,
-            link = Uri.parse("https://www.youtube.com/watch?v=s2aqkF3dJcI")
-        ),
-        BuyAndSellKinItem(
-            imageResId = R.drawable.video_sell_kin_2x,
-            buttonTextResId = R.string.action_learnHowToSellKin,
-            link = Uri.parse("https://www.youtube.com/watch?v=cyb9Da_mV9I")
-        )
-    )
+    val state by remember { viewModel.state }
 
     ConstraintLayout(
         modifier = Modifier
@@ -81,36 +70,23 @@ fun BuyAndSellKin() {
                 )
             }
 
-            items(items, key = { it.link }) { item ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            onVideoClick(
-                                context = context,
-                                link = item.link
-                            )
-                        }
-                        .padding(bottom = 10.dp)
+            items(state.items.toImmutableList(), key = { it.link }) { item ->
 
-                ) {
-                    Image(
-                        modifier = Modifier.clip(MaterialTheme.shapes.small),
-                        painter = painterResource(id = item.imageResId),
-                        contentDescription = "Video Thumbnail"
-                    )
+                VideoThumbnail(
+                    context = context,
+                    imageResId = item.imageResId,
+                    link = item.link,
+                    onVideoClick = { context, link ->
+                        viewModel.openVideo(
+                            context = context,
+                            link = link
+                        )
+                    }
+                )
 
-                    Image(
-                        modifier = Modifier
-                            .size(70.dp)
-                            .align(Alignment.Center),
-                        painter = painterResource(id = R.drawable.youtube),
-                        contentDescription = "Youtube Logo"
-                    )
-                }
                 CodeButton(
                     onClick = {
-                        onVideoClick(
+                        viewModel.openVideo(
                             context = context,
                             link = item.link
                         )
@@ -123,9 +99,9 @@ fun BuyAndSellKin() {
                     CodeButton(
                         modifier = Modifier.padding(bottom = 30.dp),
                         onClick = {
-                            shareVideo(
+                            viewModel.shareVideo(
                                 context = context,
-                                link = item.link.toString()
+                                link = item.link
                             )
                         },
                         text = stringResource(id = R.string.action_shareVideo),
@@ -138,26 +114,37 @@ fun BuyAndSellKin() {
     }
 }
 
-
-private fun onVideoClick(
+@Composable
+private fun VideoThumbnail(
     context: Context,
-    link: Uri
+    imageResId: Int,
+    link: Uri,
+    onVideoClick: (Context, Uri) -> Unit
 ) {
-    val intent = Intent(Intent.ACTION_VIEW, link)
-    if (intent.resolveActivity(context.packageManager) != null) {
-        context.startActivity(intent)
-    }
-}
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onVideoClick(context, link) }
+            .padding(bottom = 10.dp)
 
-private fun shareVideo(
-    context: Context,
-    link: String
-) {
-    val sendIntent = Intent().apply {
-        action = Intent.ACTION_SEND
-        putExtra(Intent.EXTRA_TEXT, link)
-        type = "text/plain"
+    ) {
+        Image(
+            modifier = Modifier
+                .aspectRatio(16f / 9f)
+                .fillMaxWidth()
+                .clip(MaterialTheme.shapes.small)
+                .align(Alignment.CenterStart),
+            contentScale = ContentScale.Fit,
+            painter = painterResource(id = imageResId),
+            contentDescription = "Video Thumbnail"
+        )
+
+        Image(
+            modifier = Modifier
+                .size(70.dp)
+                .align(Alignment.Center),
+            painter = painterResource(id = R.drawable.youtube),
+            contentDescription = "Youtube Logo"
+        )
     }
-    val shareIntent = Intent.createChooser(sendIntent, null)
-    context.startActivity(shareIntent)
 }
