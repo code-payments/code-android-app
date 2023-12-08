89fdff60a9ae1716d1a65e12b0dc725901422bba
Jeff Yanta <jeff@getcode.com>
2023-12-08 11:44:38 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.getcode"
-        versionCode = 293
+        versionCode = 294
         versionName = "1.1.$versionCode"
 
         minSdk = Android.minSdkVersion
