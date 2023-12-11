c19a22942898b3d98840f29154aba3b10ed489ab
Maninder T <manindersingh030@gmail.com>
2023-12-11 10:23:58 -0500

    alignment fix

--- app/src/main/java/com/getcode/view/main/account/AccountHome.kt
+++ app/src/main/java/com/getcode/view/main/account/AccountHome.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -161,7 +162,8 @@ fun ListItem(item: AccountMainItem) {
             .clickable { item.onClick() }
             .padding(vertical = 25.dp, horizontal = 25.dp)
             .fillMaxWidth()
-            .wrapContentHeight()
+            .wrapContentHeight(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             modifier = Modifier
