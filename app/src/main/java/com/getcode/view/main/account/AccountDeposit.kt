cdd6f8e9f5419637896220bf3caad35e456d3ac6
Brandon McAnsh <git@bmcreations.dev>
2024-01-16 15:09:35 -0500

    fix(settings/deposit): fillMaxSize to align button at bottom
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- app/src/main/java/com/getcode/view/main/account/AccountDeposit.kt
+++ app/src/main/java/com/getcode/view/main/account/AccountDeposit.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -50,6 +51,7 @@ fun AccountDeposit() {
     Column(
         modifier = Modifier
             .background(Brand)
+            .fillMaxSize()
             .padding(horizontal = CodeTheme.dimens.inset)
             .verticalScroll(rememberScrollState())
     ) {
