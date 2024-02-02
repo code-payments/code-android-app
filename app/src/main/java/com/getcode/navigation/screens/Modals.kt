0028aa2d42f7fed3c84ce52c7d226644b0abddf9
Brandon McAnsh <git@bmcreations.dev>
2024-02-02 09:50:54 -0500

    chore(modals): remove ModalContainer overloads outside of ModalContent interface
    overloading composable functions in interfaces is no longer supported by compiler
    
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- app/src/main/java/com/getcode/navigation/screens/Modals.kt
+++ app/src/main/java/com/getcode/navigation/screens/Modals.kt
@@ -26,116 +26,113 @@ import com.getcode.navigation.core.CodeNavigator
 import com.getcode.navigation.core.LocalCodeNavigator
 import com.getcode.theme.CodeTheme
 import com.getcode.theme.sheetHeight
-import com.getcode.util.recomposeHighlighter
 import com.getcode.view.components.SheetTitle
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
-internal interface ModalContent {
 
-    @Composable
-    fun Screen.ModalContainer(
-        closeButton: (Screen?) -> Boolean = { false },
-        screenContent: @Composable () -> Unit
+@Composable
+internal fun Screen.ModalContainer(
+    closeButton: (Screen?) -> Boolean = { false },
+    screenContent: @Composable () -> Unit
+) {
+    ModalContainer(
+        navigator = LocalCodeNavigator.current,
+        displayLogo = false,
+        backButton = { false },
+        onLogoClicked = {},
+        closeButton = closeButton,
+        screenContent = screenContent,
+    )
+}
+
+@Composable
+internal fun Screen.ModalContainer(
+    displayLogo: Boolean = false,
+    onLogoClicked: () -> Unit = { },
+    closeButton: (Screen?) -> Boolean = { false },
+    screenContent: @Composable () -> Unit
+) {
+    ModalContainer(
+        navigator = LocalCodeNavigator.current,
+        displayLogo = displayLogo,
+        backButton = { false },
+        onLogoClicked = onLogoClicked,
+        closeButton = closeButton,
+        screenContent = screenContent,
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+internal fun Screen.ModalContainer(
+    navigator: CodeNavigator = LocalCodeNavigator.current,
+    displayLogo: Boolean = false,
+    backButton: (Screen?) -> Boolean = { false },
+    onBackClicked: (() -> Unit)? = null,
+    closeButton: (Screen?) -> Boolean = { false },
+    onCloseClicked: (() -> Unit)? = null,
+    onLogoClicked: () -> Unit = { },
+    screenContent: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(sheetHeight)
     ) {
-        ModalContainer(
-            navigator = LocalCodeNavigator.current,
-            displayLogo = false,
-            backButton = { false },
-            onLogoClicked = {},
-            closeButton = closeButton,
-            screenContent = screenContent,
-        )
-    }
+        val lastItem by remember(navigator.lastModalItem) {
+            derivedStateOf { navigator.lastModalItem }
+        }
 
-    @Composable
-    fun Screen.ModalContainer(
-        displayLogo: Boolean = false,
-        onLogoClicked: () -> Unit = { },
-        closeButton: (Screen?) -> Boolean = { false },
-        screenContent: @Composable () -> Unit
-    ) {
-        ModalContainer(
-            navigator = LocalCodeNavigator.current,
+        val isBackEnabled by remember(backButton, lastItem) {
+            derivedStateOf { backButton(lastItem) }
+        }
+
+        val isCloseEnabled by remember(closeButton, lastItem) {
+            derivedStateOf { closeButton(lastItem) }
+        }
+
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val composeScope = rememberCoroutineScope()
+
+        val hideSheet = {
+            composeScope.launch {
+                keyboardController?.hide()
+                delay(500)
+                navigator.hide()
+            }
+        }
+        SheetTitle(
+            modifier = Modifier,
+            title = {
+                val name = (lastItem as? NamedScreen)?.name
+                val sheetName by remember(lastItem) {
+                    derivedStateOf { name }
+                }
+                sheetName.takeIf { !displayLogo && lastItem == this@ModalContainer }
+            },
             displayLogo = displayLogo,
-            backButton = { false },
             onLogoClicked = onLogoClicked,
-            closeButton = closeButton,
-            screenContent = screenContent,
+            // hide while transitioning to/from other destinations
+            backButton = isBackEnabled,
+            closeButton = isCloseEnabled,
+            onBackIconClicked = onBackClicked?.let { { it() } } ?: { navigator.pop() },
+            onCloseIconClicked = onCloseClicked?.let { { it() } } ?: { hideSheet() }
         )
-    }
-
-    @OptIn(ExperimentalFoundationApi::class)
-    @Composable
-    fun Screen.ModalContainer(
-        navigator: CodeNavigator = LocalCodeNavigator.current,
-        displayLogo: Boolean = false,
-        backButton: (Screen?) -> Boolean = { false },
-        onBackClicked: (() -> Unit)? = null,
-        closeButton: (Screen?) -> Boolean = { false },
-        onCloseClicked: (() -> Unit)? = null,
-        onLogoClicked: () -> Unit = { },
-        screenContent: @Composable () -> Unit
-    ) {
-        Column(
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(sheetHeight)
+                .windowInsetsPadding(WindowInsets.navigationBars)
         ) {
-            val lastItem by remember(navigator.lastModalItem) {
-                derivedStateOf { navigator.lastModalItem }
-            }
-
-            val isBackEnabled by remember(backButton, lastItem) {
-                derivedStateOf { backButton(lastItem) }
-            }
-
-            val isCloseEnabled by remember(closeButton, lastItem) {
-                derivedStateOf { closeButton(lastItem) }
-            }
-
-            val keyboardController = LocalSoftwareKeyboardController.current
-            val composeScope = rememberCoroutineScope()
-
-            val hideSheet = {
-                composeScope.launch {
-                    keyboardController?.hide()
-                    delay(500)
-                    navigator.hide()
-                }
-            }
-            SheetTitle(
-                modifier = Modifier,
-                title = {
-                    val name = (lastItem as? NamedScreen)?.name
-                    val sheetName by remember(lastItem) {
-                        derivedStateOf { name }
-                    }
-                    sheetName.takeIf { !displayLogo && lastItem == this@ModalContainer }
-                },
-                displayLogo = displayLogo,
-                onLogoClicked = onLogoClicked,
-                // hide while transitioning to/from other destinations
-                backButton = isBackEnabled,
-                closeButton = isCloseEnabled,
-                onBackIconClicked = onBackClicked?.let { { it() } } ?: { navigator.pop() },
-                onCloseIconClicked = onCloseClicked?.let { { it() } } ?: { hideSheet() }
-            )
-            Box(
-                modifier = Modifier
-                    .windowInsetsPadding(WindowInsets.navigationBars)
+            CompositionLocalProvider(
+                LocalOverscrollConfiguration provides null
             ) {
-                CompositionLocalProvider(
-                    LocalOverscrollConfiguration provides null
-                ) {
-                    screenContent()
-                }
+                screenContent()
             }
         }
     }
 }
 
+internal interface ModalContent
 internal sealed interface ModalRoot : ModalContent
 
 data object MainRoot : Screen {
