e2b1fe25f789270138ff491813b0887fe35729a2
Jeff Yanta <jeff@getcode.com>
2024-02-08 17:22:10 -0500

    Prep v1.1.302

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.getcode"
-        versionCode = 301
+        versionCode = 302
         versionName = "1.1.$versionCode"
 
         minSdk = Android.minSdkVersion
