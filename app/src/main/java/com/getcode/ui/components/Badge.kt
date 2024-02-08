770088077a3b0cc2c604da907b051db1f2e6c91d
Brandon McAnsh <git@bmcreations.dev>
2024-02-08 09:24:30 -0500

    chore: simplify badge handling to reduce size
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- app/src/main/java/com/getcode/ui/components/Badge.kt
+++ app/src/main/java/com/getcode/ui/components/Badge.kt
@@ -5,12 +5,17 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.getcode.theme.CodeTheme
+import com.getcode.ui.components.chat.ChatNodeDefaults
 import com.getcode.ui.utils.circleBackground
 
 
@@ -31,7 +36,13 @@ fun Badge(
             text = text,
             color = contentColor,
             style = CodeTheme.typography.body1.copy(fontWeight = FontWeight.W700),
-            modifier = modifier.circleBackground(color = color, padding = CodeTheme.dimens.staticGrid.x1)
+            modifier = modifier
+                .drawBehind {
+                    drawCircle(
+                        color = color,
+                        radius = this.size.maxDimension / 2f
+                    )
+                }.padding(2.dp)
         )
     }
 }
