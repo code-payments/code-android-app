84dc661abcbe43a3c19c4dd8e69f7bf003887178
Jeff Yanta <jeff@getcode.com>
2024-03-07 09:59:07 -0500

    Prep v1.1.326

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         applicationId = Android.namespace
-        versionCode = 325
+        versionCode = 326
         versionName = "1.1.$versionCode"
 
         minSdk = Android.minSdkVersion
