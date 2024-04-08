e5320146c643fb03e6fbe3f8d73a9ffebecd1841
Brandon McAnsh <git@bmcreations.dev>
2024-04-08 11:11:35 -0400

    Revert "chore: move buy module back behind beta flag"
    This reverts commit a1d353c499c8ad95b0b4b44196b1eaa9fe3dbec0.

--- api/src/main/java/com/getcode/model/PrefBool.kt
+++ api/src/main/java/com/getcode/model/PrefBool.kt
@@ -47,7 +47,7 @@ object BetaFlags {
      */
     fun canMutate(flag: PrefsBool): Boolean {
         return when (flag) {
-            PrefsBool.BUY_KIN_ENABLED -> true
+            PrefsBool.BUY_KIN_ENABLED -> false
             else -> true
         }
     }
