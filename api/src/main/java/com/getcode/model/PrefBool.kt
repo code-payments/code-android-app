a1d353c499c8ad95b0b4b44196b1eaa9fe3dbec0
Brandon McAnsh <git@bmcreations.dev>
2024-04-08 09:56:50 -0400

    chore: move buy module back behind beta flag
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- api/src/main/java/com/getcode/model/PrefBool.kt
+++ api/src/main/java/com/getcode/model/PrefBool.kt
@@ -46,7 +46,7 @@ object BetaFlags {
      */
     fun canMutate(flag: PrefsBool): Boolean {
         return when (flag) {
-            PrefsBool.BUY_KIN_ENABLED -> false
+            PrefsBool.BUY_KIN_ENABLED -> true
             else -> true
         }
     }
