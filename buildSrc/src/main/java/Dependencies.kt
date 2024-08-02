dbbde2851c37590ac474d318c6a93b44a4bae8f2
Brandon McAnsh <git@bmcreations.dev>
2024-08-02 17:00:15 -0400

    build: prep next development cycle

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Android {
     const val namespace = "com.getcode"
-    const val versionCode = 409
+    const val versionCode = 410
     const val compileSdkVersion = 34
     const val minSdkVersion = 24
     const val targetSdkVersion = 34
