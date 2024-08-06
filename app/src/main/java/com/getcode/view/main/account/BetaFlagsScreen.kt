72ff125b6bc85b0b9a83484868586703a79a011a
Brandon McAnsh <git@bmcreations.dev>
2024-08-06 13:55:15 -0400

    chore: move internal buy kin flow behind a beta flag
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- app/src/main/java/com/getcode/view/main/account/BetaFlagsScreen.kt
+++ app/src/main/java/com/getcode/view/main/account/BetaFlagsScreen.kt
@@ -30,7 +30,9 @@ fun BetaFlagsScreen(
         val titleResId: Int,
         val subtitleText: String,
         val dataState: Boolean,
-        val onChange: (Boolean) -> Unit
+        val onChange: (Boolean) -> Unit = { value ->
+            viewModel.dispatchEvent(BetaFlagsViewModel.Event.Toggle(flag, value))
+        }
     )
 
     val state by viewModel.stateFlow.collectAsState()
@@ -43,67 +45,73 @@ fun BetaFlagsScreen(
             R.string.beta_vibrate_on_scan,
             stringResource(R.string.beta_vibrate_on_scan_description),
             state.isVibrateOnScan
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.SetVibrateOnScan(it)) },
+        ),
         BetaFeature(
             PrefsBool.SHOW_CONNECTIVITY_STATUS,
             R.string.beta_network_dropoff,
             stringResource(R.string.beta_network_connectivity_description),
             state.showNetworkDropOff
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.ShowNetworkDropOff(it)) },
+        ),
         BetaFeature(
             PrefsBool.BUCKET_DEBUGGER_ENABLED,
             R.string.beta_bucket_debugger,
             stringResource(R.string.beta_bucket_debugger_description),
             state.canViewBuckets
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.UseDebugBuckets(it)) },
+        ),
         BetaFeature(
             PrefsBool.BALANCE_CURRENCY_SELECTION_ENABLED,
             R.string.beta_balance_currency,
             stringResource(R.string.beta_balance_currency_description),
             state.currencySelectionBalanceEnabled
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableCurrencySelectionInBalance(it)) },
+        ),
         BetaFeature(
             PrefsBool.GIVE_REQUESTS_ENABLED,
             R.string.beta_give_requests_mode,
             stringResource(id = R.string.beta_give_requests_description),
             state.giveRequestsEnabled
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableGiveRequests(it)) },
+        ),
         BetaFeature(
             PrefsBool.BUY_MODULE_ENABLED,
             R.string.beta_buy_kin,
             stringResource(id = R.string.beta_buy_kin_description),
             state.buyKinEnabled
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableBuyKin(it)) },
+        ),
         BetaFeature(
             PrefsBool.CHAT_UNSUB_ENABLED,
             R.string.beta_chat_unsub,
             stringResource(id = R.string.beta_chat_unsub_description),
             state.chatUnsubEnabled,
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableChatUnsubscribe(it)) },
+        ),
         BetaFeature(
             PrefsBool.TIPS_ENABLED,
             R.string.beta_tipcard,
             stringResource(id = R.string.beta_tipcard_description),
             state.tipsEnabled,
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableTipCard(it)) },
+        ),
         BetaFeature(
             PrefsBool.TIPS_CHAT_ENABLED,
             R.string.beta_tipchats,
             stringResource(id = R.string.beta_tipchats_description),
             state.tipsChatEnabled,
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableTipChats(it)) },
+        ),
         BetaFeature(
             PrefsBool.TIPS_CHAT_CASH_ENABLED,
             R.string.beta_tipchats_cash,
             stringResource(id = R.string.beta_tipchats_cash_description),
             state.tipsChatCashEnabled,
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.EnableTipsChatCash(it)) },
+        ),
+        BetaFeature(
+            PrefsBool.KADO_WEBVIEW_ENABLED,
+            R.string.beta_kado_webview,
+            stringResource(id = R.string.beta_kado_webview_description),
+            state.kadoWebViewEnabled,
+        ),
         BetaFeature(
             PrefsBool.DISPLAY_ERRORS,
             R.string.beta_display_errors,
             "",
             state.displayErrors,
-        ) { viewModel.dispatchEvent(BetaFlagsViewModel.Event.ShowErrors(it)) }
+        )
     ).filter { state.canMutate(it.flag) }
 
     LazyColumn {
