e372b358065b5fc1c9eb3b0f4f818630f82c8fe9
Brandon McAnsh <git@bmcreations.dev>
2024-09-04 10:39:12 -0400

    fix(camera): prevent possible fish eye effect after scanning a bill and returning to initial zoom
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- app/src/main/java/com/getcode/ui/utils/AnimationUtils.kt
+++ app/src/main/java/com/getcode/ui/utils/AnimationUtils.kt
@@ -2,6 +2,7 @@ package com.getcode.ui.utils
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
+import kotlin.math.pow
 
 
 object AnimationUtils {
@@ -63,6 +64,32 @@ object AnimationUtils {
             ModalAnimationSpeed.Normal -> modalEnter
             ModalAnimationSpeed.Slow -> modalEnterSlow
         }  togetherWith modalExit
+    }
+
+    fun ease(
+        value: Float,
+        fromRange: ClosedFloatingPointRange<Float>,
+        toRange: ClosedFloatingPointRange<Float>,
+        easeIn: Boolean,
+        easeOut: Boolean
+    ): Float {
+        val normalizedValue = (value - fromRange.start) / (fromRange.endInclusive - fromRange.start)
+
+        val easedValue: Float = if (easeIn && easeOut) {
+            if (normalizedValue < 0.5f) {
+                4 * normalizedValue * normalizedValue * normalizedValue
+            } else {
+                1 - (-2 * normalizedValue + 2).toDouble().pow(3.0).toFloat() / 2
+            }
+        } else if (easeIn) {
+            normalizedValue * normalizedValue * normalizedValue
+        } else if (easeOut) {
+            1 - (1 - normalizedValue).toDouble().pow(3.0).toFloat()
+        } else {
+            normalizedValue
+        }
+
+        return easedValue * (toRange.endInclusive - toRange.start) + toRange.start
     }
 }
 
