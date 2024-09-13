028507eb4267fb63499b5a3f4240255c3940d9c5
Brandon McAnsh <git@bmcreations.dev>
2024-09-13 10:28:35 -0400

    feat: add autosizing to our TextInput
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- app/src/main/java/com/getcode/ui/components/TextInput.kt
+++ app/src/main/java/com/getcode/ui/components/TextInput.kt
@@ -1,6 +1,5 @@
 package com.getcode.ui.components
 
-import android.graphics.Paint.Align
 import android.view.ViewTreeObserver
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
@@ -17,16 +16,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text2.BasicSecureTextField
 import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.foundation.text2.input.TextFieldLineLimits
 import androidx.compose.foundation.text2.input.TextFieldState
-import androidx.compose.foundation.text2.input.TextObfuscationMode
 import androidx.compose.foundation.text2.input.textAsFlow
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldColors
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -34,19 +29,20 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -54,10 +50,16 @@ import com.getcode.theme.BrandLight
 import com.getcode.theme.CodeTheme
 import com.getcode.theme.extraSmall
 import com.getcode.theme.inputColors
+import com.getcode.ui.utils.measured
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
+sealed interface AutoSize {
+    data object Disabled: AutoSize
+    data class Constrained(val minimum: TextStyle): AutoSize
+}
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TextInput(
     modifier: Modifier = Modifier,
@@ -75,7 +77,9 @@ fun TextInput(
     shape: Shape = CodeTheme.shapes.extraSmall,
     colors: TextFieldColors = inputColors(),
     enabled: Boolean = true,
+    isError: Boolean = false,
     readOnly: Boolean = false,
+    autosize: AutoSize = AutoSize.Disabled,
     leadingIcon: (@Composable () -> Unit)? = null,
     trailingIcon: (@Composable () -> Unit)? = null,
     scrollState: ScrollState = rememberScrollState(),
@@ -85,42 +89,73 @@ fun TextInput(
     val placeholderColor by colors.placeholderColor(enabled = enabled)
     val borderColor by colors.indicatorColor(
         enabled = enabled,
-        isError = false,
+        isError = isError,
         interactionSource = remember { MutableInteractionSource() }
     )
 
-    BasicTextField2(
-        modifier = modifier
-            .background(backgroundColor, shape)
-            .defaultMinSize(minHeight = minHeight),
-        enabled = enabled,
-        readOnly = readOnly,
-        state = state,
-        cursorBrush = SolidColor(colors.cursorColor(isError = false).value),
-        keyboardOptions = keyboardOptions,
-        keyboardActions = keyboardActions,
-        textStyle = style.copy(color = textColor),
-        lineLimits = if (maxLines == 1) {
-            TextFieldLineLimits.SingleLine
-        } else {
-            TextFieldLineLimits.MultiLine(minHeightInLines = minLines, maxHeightInLines = maxLines)
-        },
-        decorator = {
-            DecoratorBox(
-                state = state,
-                placeholder = placeholder,
-                placeholderStyle = placeholderStyle,
-                placeholderColor = placeholderColor,
-                borderColor = borderColor,
-                leadingIcon = leadingIcon,
-                trailingIcon = trailingIcon,
-                contentPadding = contentPadding,
-                shape = shape,
-                innerTextField = it
-            )
-        },
-        scrollState = scrollState
-    )
+    var textSize by remember { mutableStateOf(style.fontSize) }
+    var textFieldSize by remember { mutableStateOf(DpSize.Zero) }
+    var textLayoutResult by remember { mutableStateOf<TextLayoutResult?>(null) }
+
+    val density = LocalDensity.current
+    // Adjust font size based on textField size
+    LaunchedEffect(textFieldSize, textLayoutResult, state.text) {
+        when (autosize) {
+            is AutoSize.Constrained -> {
+                val text = state.text
+                if (textFieldSize.width > 0.dp && text.isNotEmpty()) {
+                    // Calculate an approximate font size based on the available width and text length
+                    val newFontSize = with (density) { (textFieldSize.width / text.length * 1.2f).toSp() }
+                        .coerceAtMost(style.fontSize)
+                        .coerceAtLeast(autosize.minimum.fontSize)
+                    textSize = newFontSize
+                }
+            }
+            AutoSize.Disabled -> Unit
+        }
+    }
+
+
+    Box(modifier = modifier.measured { textFieldSize = it }) {
+        BasicTextField2(
+            modifier = Modifier
+                .background(backgroundColor, shape)
+                .defaultMinSize(minHeight = minHeight),
+            enabled = enabled,
+            readOnly = readOnly,
+            state = state,
+            cursorBrush = SolidColor(colors.cursorColor(isError = false).value),
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            textStyle = style.copy(color = textColor, fontSize = textSize),
+            lineLimits = if (maxLines == 1) {
+                TextFieldLineLimits.SingleLine
+            } else {
+                TextFieldLineLimits.MultiLine(
+                    minHeightInLines = minLines,
+                    maxHeightInLines = maxLines
+                )
+            },
+            onTextLayout = {
+                textLayoutResult = it()
+            },
+            decorator = {
+                DecoratorBox(
+                    state = state,
+                    placeholder = placeholder,
+                    placeholderStyle = placeholderStyle,
+                    placeholderColor = placeholderColor,
+                    borderColor = borderColor,
+                    leadingIcon = leadingIcon,
+                    trailingIcon = trailingIcon,
+                    contentPadding = contentPadding,
+                    shape = shape,
+                    innerTextField = it
+                )
+            },
+            scrollState = scrollState
+        )
+    }
 
     LaunchedEffect(Unit) {
         state.textAsFlow()
@@ -199,4 +234,12 @@ fun keyboardAsState(): State<Boolean> {
         onDispose { viewTreeObserver.removeOnGlobalLayoutListener(listener) }
     }
     return keyboardState
+}
+
+fun TextUnit.coerceAtMost(other: TextUnit): TextUnit {
+    return if (this > other) other else this
+}
+
+fun TextUnit.coerceAtLeast(other: TextUnit): TextUnit {
+    return if (this < other) other else this
 }
