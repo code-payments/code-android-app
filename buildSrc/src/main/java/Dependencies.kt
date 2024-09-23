2902e81e78eb947f06dc22039ea3b7e7827f293e
Brandon McAnsh <git@bmcreations.dev>
2024-09-23 14:11:24 -0400

    build: prep next development cycle

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -12,7 +12,7 @@ object Android {
 object Packaging {
     private const val majorVersion = 2
     private const val minorVersion = 1
-    private const val patchVersion = 13
+    private const val patchVersion = 14
 
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
 }
