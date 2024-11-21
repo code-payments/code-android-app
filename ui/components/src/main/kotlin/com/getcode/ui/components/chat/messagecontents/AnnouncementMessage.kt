50b0650960e28a88c4958456d74c39f069868292
Brandon McAnsh <git@bmcreations.dev>
2024-11-21 14:25:34 -0500

    chore(fc): reset contentPadding for announcement message pills back to default
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- ui/components/src/main/kotlin/com/getcode/ui/components/chat/messagecontents/AnnouncementMessage.kt
+++ ui/components/src/main/kotlin/com/getcode/ui/components/chat/messagecontents/AnnouncementMessage.kt
@@ -1,13 +1,7 @@
 package com.getcode.ui.components.chat.messagecontents
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,28 +21,8 @@ internal fun AnnouncementMessage(
                 .widthIn(max = maxWidth * 0.78f),
             text = text,
             textStyle = CodeTheme.typography.caption.copy(textAlign = TextAlign.Center),
-            contentPadding = PaddingValues(CodeTheme.dimens.grid.x1),
             backgroundColor = CodeTheme.colors.surfaceVariant,
             contentColor = CodeTheme.colors.textSecondary
         )
-//        Box(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-//            Pill(
-//                text = date,
-//                backgroundColor = CodeTheme.colors.surfaceVariant
-//            )
-//        }
-//        Column(
-//            modifier = Modifier
-//                .align(Alignment.Center)
-//                .widthIn(max = maxWidth * 0.78f),
-//            verticalArrangement = Arrangement.Center
-//        ) {
-//            Text(
-//                text = text,
-//                textAlign = TextAlign.Center,
-//                style = CodeTheme.typography.caption,
-//                color = CodeTheme.colors.textSecondary
-//            )
-//        }
     }
 }
