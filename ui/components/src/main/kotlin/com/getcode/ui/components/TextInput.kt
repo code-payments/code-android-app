c82e1162e903bc9623a694ac947f8c53ecb87e08
Brandon McAnsh <git@bmcreations.dev>
2024-12-05 07:23:36 -0500

    chore(ui): add KeyboardActionHandler to contract
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- ui/components/src/main/kotlin/com/getcode/ui/components/TextInput.kt
+++ ui/components/src/main/kotlin/com/getcode/ui/components/TextInput.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.KeyboardActionHandler
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material.Text
@@ -78,6 +79,7 @@ fun TextInput(
     contentPadding: PaddingValues = PaddingValues(),
     onStateChanged: () -> Unit = { },
     keyboardOptions: KeyboardOptions = KeyboardOptions(),
+    onKeyboardAction: KeyboardActionHandler? = null,
     style: TextStyle = CodeTheme.typography.textMedium,
     placeholderStyle: TextStyle = CodeTheme.typography.textMedium,
     shape: Shape = CodeTheme.shapes.extraSmall,
@@ -124,6 +126,7 @@ fun TextInput(
             state = state,
             cursorBrush = SolidColor(colors.cursorColor(isError = false).value),
             keyboardOptions = keyboardOptions,
+            onKeyboardAction = onKeyboardAction,
             textStyle = style.copy(color = textColor, fontSize = textSize),
             lineLimits = if (maxLines == 1) {
                 TextFieldLineLimits.SingleLine
