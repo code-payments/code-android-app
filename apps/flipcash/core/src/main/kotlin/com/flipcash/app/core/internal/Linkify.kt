9538d32245a44fd0898556d8c8b66528781424d2
Brandon McAnsh <git@bmcreations.dev>
2025-04-24 12:19:01 -0400

    chore(flipcash): update cashlink subdomain
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- apps/flipcash/core/src/main/kotlin/com/flipcash/app/core/internal/Linkify.kt
+++ apps/flipcash/core/src/main/kotlin/com/flipcash/app/core/internal/Linkify.kt
@@ -3,6 +3,6 @@ package com.flipcash.app.core.internal
 import com.getcode.utils.urlEncode
 
 internal object Linkify {
-    fun cashLink(entropy: String): String = "https://cash.flipcash.com/c/#/e=${entropy}"
+    fun cashLink(entropy: String): String = "https://send.flipcash.com/c/#/e=${entropy}"
     fun tweet(message: String): String = "https://www.twitter.com/intent/tweet?text=${message.urlEncode()}"
 }
