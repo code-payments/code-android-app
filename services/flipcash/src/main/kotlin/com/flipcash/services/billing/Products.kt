a33cfef2d5eb4bdd06de3cd587609356e3f8c670
Brandon McAnsh <git@bmcreations.dev>
2025-05-07 09:55:38 -0400

    chore(services/flipcash): lowercase IAP product IDs for Google
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- services/flipcash/src/main/kotlin/com/flipcash/services/billing/Products.kt
+++ services/flipcash/src/main/kotlin/com/flipcash/services/billing/Products.kt
@@ -1,6 +1,6 @@
 package com.flipcash.services.billing
 
 enum class IapProduct(internal val productId: String, internal val isConsumable: Boolean) {
-    CreateAccount("com.flipcash.iap.createAccount", true),
-    CreateAccountWithWelcomeBonus("com.flipcash.iap.createAccountWithWelcomeBonus", false)
+    CreateAccount("com.flipcash.iap.createaccount", true),
+    CreateAccountWithWelcomeBonus("com.flipcash.iap.createaccountwithwelcomebonus", false)
 }
