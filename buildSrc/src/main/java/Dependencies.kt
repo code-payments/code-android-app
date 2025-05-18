7b886352f655cca359a563440b09919aa4b60495
Brandon McAnsh <git@bmcreations.dev>
2025-05-17 21:12:52 -0400

    chore: bump flipcash version for internal build
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -29,7 +29,7 @@ sealed class Packaging(
     object Flipcash: Packaging(
         majorVersion = 0,
         minorVersion = 0,
-        patchVersion = 2,
+        patchVersion = 3,
     )
 
     object Flipchat: Packaging(
