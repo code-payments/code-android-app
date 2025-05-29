203aaf5c1d347875fadfd49332758cc39805584b
Brandon McAnsh <git@bmcreations.dev>
2025-05-29 11:35:04 -0400

    build: update patchVersion to mark product meeting
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -28,7 +28,7 @@ sealed class Packaging(
     object Flipcash: Packaging(
         majorVersion = 0,
         minorVersion = 4,
-        patchVersion = 20250526,
+        patchVersion = 20250529,
     )
 
     object Flipchat: Packaging(
