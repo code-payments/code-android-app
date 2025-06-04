0c83a5cec9dfee5694fd683c2705b517acd33d1b
Brandon McAnsh <git@bmcreations.dev>
2025-06-03 20:35:23 -0400

    build: move packaging to own file
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

---
+++ buildSrc/src/main/java/Packaging.kt
@@ -0,0 +1,35 @@
+sealed class Packaging(
+    majorVersion: String,
+    minorVersion: String,
+    patchVersion: String,
+) {
+    constructor(
+        majorVersion: Int,
+        minorVersion: Int,
+        patchVersion: Int
+    ) : this(
+        majorVersion = majorVersion.toString(),
+        minorVersion = minorVersion.toString(),
+        patchVersion = patchVersion.toString()
+    )
+
+    val versionName = "$majorVersion.$minorVersion.$patchVersion"
+
+    object Code : Packaging(
+        majorVersion = 2,
+        minorVersion = 1,
+        patchVersion = 14,
+    )
+
+    object Flipcash : Packaging(
+        majorVersion = 2025, // release year
+        minorVersion = 6, // release month
+        patchVersion = 2, // cycle in minor version
+    )
+
+    object Flipchat : Packaging(
+        majorVersion = 1,
+        minorVersion = 0,
+        patchVersion = 10,
+    )
+}
