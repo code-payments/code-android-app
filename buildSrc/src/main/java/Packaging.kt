654cef7eff27a472fefe46c273d08949bf628a92
Brandon McAnsh <git@bmcreations.dev>
2025-06-11 10:01:14 -0400

    build(flipcash): bump versioning post release
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- buildSrc/src/main/java/Packaging.kt
+++ buildSrc/src/main/java/Packaging.kt
@@ -24,7 +24,7 @@ sealed class Packaging(
     object Flipcash : Packaging(
         majorVersion = 2025, // release year
         minorVersion = 6, // release month
-        patchVersion = 7, // cycle in minor version
+        patchVersion = 8, // cycle in minor version
     )
 
     object Flipchat : Packaging(
