47aeaf90da78793e69e1a9592259248fcb8365f5
Brandon McAnsh <git@bmcreations.dev>
2026-03-22 12:36:59 -0400

    chore(settings): update user "region" to reflect balance currency
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- services/flipcash/src/main/kotlin/com/flipcash/services/controllers/SettingsController.kt
+++ services/flipcash/src/main/kotlin/com/flipcash/services/controllers/SettingsController.kt
@@ -2,22 +2,31 @@ package com.flipcash.services.controllers
 
 import com.flipcash.services.repository.SettingsRepository
 import com.flipcash.services.user.UserManager
+import com.getcode.opencode.exchange.Exchange
 import com.getcode.util.locale.LocaleHelper
+import com.getcode.utils.trace
 import javax.inject.Inject
 
 class SettingsController @Inject constructor(
     private val repository: SettingsRepository,
     private val localeHelper: LocaleHelper,
     private val userManager: UserManager,
+    private val exchange: Exchange,
 ) {
     suspend fun update(): Result<Unit> {
         val locale = localeHelper.getLanguageTag()
-        val region = localeHelper.getDefaultCurrencyName()
-
-        println("locale=$locale, region=$region")
+        val currencyRegion = exchange.balanceRate.currency
         val owner = userManager.accountCluster?.authority?.keyPair
             ?: return Result.failure(Throwable("No account cluster in UserManager"))
 
-        return repository.update(locale, region, owner)
+        trace(
+            tag = "Settings",
+            message = "Updating settings",
+            metadata = {
+                "locale" to locale
+                "region" to currencyRegion.name
+            }
+        )
+        return repository.update(locale, currencyRegion.name, owner)
     }
 }
