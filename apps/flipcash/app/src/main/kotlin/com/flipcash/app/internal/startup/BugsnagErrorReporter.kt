cb71147874d8f7585990b802931049f884588cd4
Brandon McAnsh <git@bmcreations.dev>
2026-04-09 14:02:01 -0400

    chore: simplify handling of errors dispatched to busnag
    Reduce volume of errors sent to bugsnag to only allow actionable, unexpected errors (notifiable)
    
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- apps/flipcash/app/src/main/kotlin/com/flipcash/app/internal/startup/BugsnagErrorReporter.kt
+++ apps/flipcash/app/src/main/kotlin/com/flipcash/app/internal/startup/BugsnagErrorReporter.kt
@@ -5,17 +5,8 @@ import com.getcode.utils.ErrorReporter
 
 class BugsnagErrorReporter : ErrorReporter {
     override fun report(error: Throwable, cause: Throwable, isNotifiable: Boolean) {
+        if (!isNotifiable) return
         if (!Bugsnag.isStarted()) return
-        Bugsnag.notify(error) { event ->
-            if (isNotifiable) {
-                event.addMetadata("alert", "slack_notify", true)
-                event.addMetadata("alert", "error_type", cause.javaClass.simpleName)
-                event.addMetadata(
-                    "alert", "error_family",
-                    cause.javaClass.enclosingClass?.simpleName ?: "Unknown"
-                )
-            }
-            true
-        }
+        Bugsnag.notify(error)
     }
 }
