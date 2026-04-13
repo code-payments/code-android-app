5dd63b5b9f7eb8f5d254c564470b794df3ace68c
Brandon McAnsh <git@bmcreations.dev>
2026-04-13 13:21:48 -0400

    chore: launch currency discovery
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- apps/flipcash/shared/featureflags/src/main/kotlin/com/flipcash/app/featureflags/FeatureFlag.kt
+++ apps/flipcash/shared/featureflags/src/main/kotlin/com/flipcash/app/featureflags/FeatureFlag.kt
@@ -120,8 +120,8 @@ sealed interface FeatureFlag {
     @FeatureFlagMarker
     data object TokenDiscovery: FeatureFlag {
         override val key: String = "token_discovery_enabled"
-        override val default: Boolean = false
-        override val launched: Boolean = false
+        override val default: Boolean = true
+        override val launched: Boolean = true
         override val visible: Boolean = true
         override val persistLogOut: Boolean = false
     }
