19573c2d4885f1764d7296cb7bce820faeb99438
Brandon McAnsh <git@bmcreations.dev>
2026-04-22 20:53:54 -0400

    feat: launch currency creator
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- apps/flipcash/shared/featureflags/src/main/kotlin/com/flipcash/app/featureflags/FeatureFlag.kt
+++ apps/flipcash/shared/featureflags/src/main/kotlin/com/flipcash/app/featureflags/FeatureFlag.kt
@@ -75,8 +75,8 @@ sealed interface FeatureFlag {
     @FeatureFlagMarker
     data object CurrencyCreator: FeatureFlag {
         override val key: String = "currency_creator_enabled"
-        override val default: Boolean = false
-        override val launched: Boolean = false
+        override val default: Boolean = true
+        override val launched: Boolean = true
         override val visible: Boolean = true
         override val persistLogOut: Boolean = false
     }
