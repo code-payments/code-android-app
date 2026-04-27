c56bc334e17b92738633030b5c7a0efa364f9204
Brandon McAnsh <git@bmcreations.dev>
2026-04-26 21:25:37 -0400

    build: migrate to gradle jvm daemon
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,9 @@ pluginManagement {
         }
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.PREFER_SETTINGS
