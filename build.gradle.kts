59c41ae9d84946ac570f7256754fd06bd4c53f72
Brandon McAnsh <git@bmcreations.dev>
2026-05-11 16:28:45 -0400

    chore: migrate to AGP built-in Kotlin compilation
    Remove org.jetbrains.kotlin.android plugin in favor of AGP 9's built-in
    Kotlin support. Upgrade Hilt to 2.59.2 for AGP 9 new DSL compatibility
    and switch convention plugins to com.android.build.api.dsl.LibraryExtension.
    Add explicit kotlin-test-junit dependency since AGP's built-in Kotlin
    does not auto-select the JUnit variant for kotlin("test").
    
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.ksp) apply false
