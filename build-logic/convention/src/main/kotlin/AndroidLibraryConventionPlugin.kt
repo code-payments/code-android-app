551bf7ef4fb780a1081373f258c5b14a297480aa
Brandon McAnsh <git@bmcreations.dev>
2026-06-03 12:02:06 -0400

    fix(build): allow modules without tests to pass testDebugUnitTest
    Gradle 9 fails test tasks when test dependencies exist but no test
    classes are discovered. Set failOnNoDiscoveredTests to false in the
    convention plugin since kotlin-test-junit is added to all modules.

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,12 +1,14 @@
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
+import org.gradle.api.tasks.testing.Test
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
@@ -58,6 +60,10 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                         "kotlin.RequiresOptIn"
                     )
                 }
+            }
+
+            tasks.withType<Test> {
+                failOnNoDiscoveredTests.set(false)
             }
 
             dependencies {
