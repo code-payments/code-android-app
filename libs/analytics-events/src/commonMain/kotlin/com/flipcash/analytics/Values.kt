0e742c9a4714127cf038f8dc309d33c7dcc48853
Brandon McAnsh <git@bmcreations.dev>
2026-09-23 19:39:24 -0400

    refactor(analytics-events): move buttons into the catalogue

--- libs/analytics-events/src/commonMain/kotlin/com/flipcash/analytics/Values.kt
+++
@@ -1,8 +0,0 @@
-package com.flipcash.analytics
-
-enum class Button(val value: String) {
-    CREATE_ACCOUNT("Create Account"), SAVE_ACCESS_KEY("Save Access Key"), WROTE_ACCESS_KEY("Wrote Access Key"),
-    ALLOW_PUSH("Allow Push"), SKIP_PUSH("Skip Push"), ALLOW_CONTACTS("Allow Contacts"), SKIP_CONTACTS("Skip Contacts"),
-    BUY_WITH_RESERVES("Buy With Reserves"), BUY_WITH_PHANTOM("Buy With Phantom"), BUY_WITH_COINBASE("Buy With Coinbase"),
-    BUY_WITH_OTHER_WALLET("Buy With Other Wallet"), SHARE_TOKEN_INFO("Share Token Info"),
-}
--- libs/analytics-events/src/commonMain/kotlin/com/flipcash/analytics/events/ButtonEvents.kt
+++
@@ -1,8 +0,0 @@
-package com.flipcash.analytics.events
-
-import com.flipcash.analytics.Button
-import com.flipcash.analytics.event
-
-object ButtonEvents {
-    fun tapped(button: Button) = event("Button: ${button.value}")
-}
