24ebf561c919f2cc5a10f7cf69f8cb5413bc9689
Brandon McAnsh <git@bmcreations.dev>
2026-09-23 19:39:54 -0400

    refactor(analytics-events): move people counters into the catalogue

--- libs/analytics-events/src/commonMain/kotlin/com/flipcash/analytics/PeopleCounter.kt
+++
@@ -1,8 +0,0 @@
-package com.flipcash.analytics
-
-/** Cumulative per-user counters, stored as Mixpanel people properties. */
-enum class PeopleCounter(val key: String) {
-    TIPS("Tips Received"),
-    TIPS_VALUE("Tips Received Value"),
-    MESSAGES("Messages Received"),
-}
