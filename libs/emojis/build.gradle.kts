5f24d1f4948265530731983c48c8289dc7581278
Brandon McAnsh <git@bmcreations.dev>
2026-09-25 18:14:39 -0400

    fix(emojis): run the Hilt processor so the app sees ReactionsModule

--- libs/emojis/build.gradle.kts
+++ libs/emojis/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.flipcash.android.library)
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -8,6 +10,7 @@ android {
 
 dependencies {
     implementation(libs.bundles.hilt)
+    ksp(libs.bundles.hilt.compiler)
     implementation(libs.bundles.kotlinx.serialization)
     implementation(libs.kotlinx.datetime)
     implementation(libs.androidx.datastore)
